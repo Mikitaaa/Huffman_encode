96f61ffcc84607c265b7125490750a9e433e328d
Mikitaaa <nik.podterob@gmail.com>
2023-02-12 21:09:55 +0300

    Initial Commit

---
+++ hapfman_code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hapfman_code
+//
+//  Created by Никита Подтероб on 12.02.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
